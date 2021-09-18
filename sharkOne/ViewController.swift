31f6c8bdffccd6fc6b3778b939a4ddb3daed5f3c
Sergey <piolersa85@mail.ru>
2021-09-18 14:22:01 +0200

    Initial Commit

---
+++ sharkOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sharkOne
+//
+//  Created by Sergey on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
